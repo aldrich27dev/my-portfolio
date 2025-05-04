a0d5ec41e9b27f6bfeb32915d7dfc372746488f4
aldrich27dev <129347457+aldrich27dev@users.noreply.github.com>
2025-05-04 22:45:01 +0800

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()]
+  site: 'https://aldrich27dev.github.io',
+  base: 'my-portfolio',
+
 });
